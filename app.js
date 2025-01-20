385855406de451b956af1e407555ffb1d2bd2774
OrzeFox <dacm1997@yahoo.com>
2025-01-20 21:49:02 +0100

    add sortearAmigo function to randomly select a friend from the list

--- app.js
+++ app.js
@@ -9,17 +9,21 @@ function cleanInput (friendNameInput){
 	friendNameInput.value = '';
 };
 function validateName (name) {
+
 	if(name.length < 1) {
 		alert('Por favor, inserte un nombre.');
 	} else {
 		addName(name);
 		console.log(friends);
 	}
+	
 };
 
 
 function updateFriendList () {
+
 	const friendList = document.getElementById('listaAmigos');
+
 	friendList.innerHTML = '';
 	for( let friend of friends){
         const listName = document.createElement('li');
@@ -29,8 +33,25 @@ function updateFriendList () {
 }
 
 function agregarAmigo () {
+
 	const friendNameInput = document.getElementById('amigo');
+
 	validateName(friendNameInput.value);
 	cleanInput(friendNameInput);
 	updateFriendList();
 };
+
+function sortearAmigo () {
+
+	if(friends.length < 1){
+		return alert('introduce almenos 2 nombres');
+	}
+	const resultado = document.getElementById('resultado');
+	const selectedName = Math.floor(Math.random() * friends.length)
+	const resultName = document.createElement('p');
+
+	resultado.innerHTML = '';
+	resultName.textContent = friends[selectedName];
+	resultado.appendChild(resultName);
+
+}
