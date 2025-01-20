623d2cf32867d3b7c89a3f95251c6df9af03786f
OrzeFox <dacm1997@yahoo.com>
2025-01-20 21:05:21 +0100

    create agregar amigo function

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let friends = [];
+
+function addName (name)  {
+	friends.push(name);
+};
+function cleanInput (firendNameInput){
+	firendNameInput.value = '';
+};
+function validateName (name) {
+	if(name.length < 1) {
+		alert('Por favor, inserte un nombre.');
+	} else {
+		addName(name);
+		console.log(friends);
+	}
+};
+
+function agregarAmigo () {
+	const firendNameInput = document.getElementById('amigo');
+	validateName(firendNameInput.value);
+	cleanInput(firendNameInput);
+};
